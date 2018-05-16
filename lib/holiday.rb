d428438f154b377674e42efc465f4fe605350365
Conor Hamilton <conhamil19@gmail.com>
2018-05-16 13:27:36 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,7 +73,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holiday_and_supply|
     holiday_and_supply.each do |holiday, supply|
       if supply.include?("BBQ")
-        holiday
+       return holiday
       end
     end
 end
